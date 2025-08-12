8a4bae18367ce7509b5856c8a36209fd7aa68ee8
Yugandhar Babu K <yugeyugandharbabu@gmail.com>
2025-08-12 10:35:51 +0530

    src added

---
+++ src/ugv_gazebo/launch/display.launch.py
@@ -0,0 +1,42 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ugv_gazebo_path = get_package_share_directory('ugv_gazebo')
+    xacro_path = os.path.join(ugv_gazebo_path, 'urdf', 'ugv_gazebo_plugins.xacro')
+
+    # Process xacro to URDF string
+    robot_desc_config = xacro.process_file(xacro_path)
+    robot_desc = robot_desc_config.toxml()
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={'verbose':'true'}.items()
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_desc, 'use_sim_time': True}],
+        output='screen'
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'ugv', '-topic', 'robot_description'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher_node,
+        spawn_entity,
+    ])
